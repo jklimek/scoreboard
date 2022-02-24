be2afb153a23131137bd3c31a2ccc82d19d62570
jklimek <klimek.jakub@gmail.com>
2022-02-25 00:52:40 +0100

    Ignore tvboard

--- scores_html/app.py
+++ scores_html/app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/board')
-def board():
-    return render_template('board.html')
+# @app.route('/board')
+# def board():
+#     return render_template('board.html')
 
 
 @app.route('/controller')
