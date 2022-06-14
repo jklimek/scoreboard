f3fbdccda4d2257f0da47697e71fcf66bb9babc6
jklimek <klimek.jakub@gmail.com>
2022-06-14 21:49:22 +0200

    Clear easeIn

--- scores_html/static/js/scoreboard.js
+++ scores_html/static/js/scoreboard.js
@@ -179,21 +179,12 @@ function windChange(windAngleTarget) {
         duration: 500,
         easing: 'swing',
         step: function (now, fx) {
-            // console.log((easeInOutBack(fx.pos)));
             windArrowHandle.css({transform: 'rotate(' + (this.rotation - 45) % 360 + 'deg)'});
         }
     });
     windAngle = windAngleTarget
 }
 
-function easeInOutBack(x) {
-    const c1 = 1.70158;
-    const c2 = c1 * 1.525;
-
-    return x < 0.5
-        ? (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
-        : (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
-}
 
 function timeout(team) {
     setAssistAndScorerTexts("", "TIMEOUT");
