7f532e104f11365b3f767e94fa4e0737915b8d0f
jklimek <klimek.jakub@gmail.com>
2025-04-18 23:01:28 +0200

    Revert "Refactor game server: modularize event handling and statistics calculation, improve utility functions"
    This reverts commit cbfed69ffe5f548f3629ad7eb2477e1e51e631ca.

--- scores_server/models/game_broadcast.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def send_message_to_all(clients, message, logger=None):
-    if logger:
-        logger.debug(f"Send ws message: {message}")
-    for client in clients:
-        client.sendMessage(json.dumps(message))
